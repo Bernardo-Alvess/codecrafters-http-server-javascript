4835b20b3cbdb4d3281daf2df8534bc9637ca1a6
Bernardo Alves <bernardoalves330@gmail.com>
2024-07-26 20:54:53 -0300

    test

--- app/main.js
+++ app/main.js
@@ -32,7 +32,7 @@ const server = net.createServer((socket) => {
                 socket.write(response)
             }else if(path === '/user-agent'){
                 console.log('/user-agent')
-                console.log(headerLines)
+                console.log(`headerlines = ` + headerLines[2])
                 const body = headerLines[2].split(' ')[1]
                 console.log('body: ' + body)
                 const header = mountHeader(body)
