c8f69a4b11f2ce10c89861b3261e50635cdf77b9
Bernardo Alves <bernardoalves330@gmail.com>
2024-07-26 21:43:17 -0300

    teste

--- app/main.js
+++ app/main.js
@@ -1,12 +1,9 @@
 const net = require("net");
 const fs = require('fs');
-const path = require('path');
 const { mountHeader } = require("./util/headers");
 const responseOk = 'HTTP/1.1 200 OK';
 const responseNotFound = 'HTTP/1.1 404 Not Found';
 
-// Set the base directory for file storage
-const baseDirectory = __dirname; // This should match the directory used for testing
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
@@ -32,23 +29,15 @@ const server = net.createServer((socket) => {
                 const header = mountHeader(body);
                 const response = `${responseOk}\r\n${header}\r\n${body}`;
                 socket.write(response);
-            } else if (url.includes('/files/')) {
-                const fileName = url.split('/files/')[1]; // Extract filename
-                const filePath = path.join(baseDirectory, 'files/', fileName); // Construct full file path
-                console.log(`Checking file path: ${filePath}`);
-
-                if (fs.existsSync(filePath)) {
-                    console.log('File exists, sending response');
-                    const fileContent = fs.readFileSync(filePath); // Read file contents
-                    const contentLength = fileContent.length;
-                    const header = `Content-Type: application/octet-stream\r\nContent-Length: ${contentLength}`;
-
-                    // Send the response with file content
-                    const response = `${responseOk}\r\n${header}\r\n\r\n${fileContent}`;
-                    socket.write(response);
+            }else if (url.startsWith("/files/")) {
+                const directory = process.argv[3];
+                const filename = url.split("/files/")[1];
+                if (fs.existsSync(`${directory}/${filename}`)) {
+                  const content = fs.readFileSync(`${directory}/${filename}`).toString();
+                  const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`;
+                  socket.write(res);
                 } else {
-                    console.log('File not found, sending 404');
-                    socket.write(`${responseNotFound}\r\n\r\n`);
+                  socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
                 }
             } else {
                 socket.write(`${responseNotFound}\r\n\r\n`);
