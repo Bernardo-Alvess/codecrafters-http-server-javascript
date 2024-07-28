9bd6fe6c43baf43b17db9f824b37505ca91fa142
Bernardo Alves <bernardoalves330@gmail.com>
2024-07-28 18:52:23 -0300

    create file

--- app/main.js
+++ app/main.js
@@ -54,7 +54,7 @@
 //                 }catch(e){
 //                     socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
 //                 }
-                
+
 
 //             } else {
 //                 socket.write(`${responseNotFound}\r\n\r\n`)
@@ -93,52 +93,63 @@ const server = net.createServer((socket) => {
 
         const request = buffer.split('\r\n\r\n')[0];
         if (request) {
-            const [requestLine,...headerLines] = request.split('\n')
+            const [requestLine, ...headerLines] = request.split('\n')
             const [method, url] = requestLine.split(' ')
 
             console.log(`Request Line: ${requestLine}`)
             console.log(`Method: ${method}`)
             console.log(`URL: ${url}`)
 
-            if (method === 'GET') {
-                if (url === '/') {
-                    socket.write(`${responseOk}\r\n\r\n`)
+            switch (method) {
+                case 'GET':
+                    if (url === '/') {
+                        socket.write(`${responseOk}\r\n\r\n`)
 
-                } else if (url.includes('/echo/')) {
-                    const content = url.split('/echo/')[1]
-                    const header = mountHeader(content)
-                    const response = `${responseOk}\r\n${header}\r\n${content}`
+                    } else if (url.includes('/echo/')) {
+                        const content = url.split('/echo/')[1]
+                        const header = mountHeader(content)
+                        const response = `${responseOk}\r\n${header}\r\n${content}`
 
-                    socket.write(response)
+                        socket.write(response)
 
-                } else if (url === '/user-agent') {
-                    const body = headerLines[1].split(' ')[1]
-                    const header = mountHeader(body)
-                    const response = `${responseOk}\r\n${header}\r\n${body}`
+                    } else if (url === '/user-agent') {
+                        const body = headerLines[1].split(' ')[1]
+                        const header = mountHeader(body)
+                        const response = `${responseOk}\r\n${header}\r\n${body}`
 
-                    socket.write(response)
+                        socket.write(response)
 
-                } else if (url.startsWith("/files/")) {
-                    try{
+                    } else if (url.startsWith("/files/")) {
+                        try {
+                            const directory = process.argv[3] || '/tmp/'
+                            const filename = url.split("/files/")[1]
+                            const isFileValid = fs.existsSync(`${directory}/${filename}`)
+
+                            if (!isFileValid) {
+                                socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+                            } else {
+                                const content = fs.readFileSync(`${directory}/${filename}`)
+                                const header = mountHeader(content)
+                                const response = `${responseOk}\r\n${header}\r\n${content.toString()}`
+
+                                socket.write(response)
+                            }
+                        } catch (e) {
+                            socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+                        }
+                    } else {
+                        socket.write(`${responseNotFound}\r\n\r\n`)
+                    }
+                case 'POST':
+                    if (url.includes('/files/')) {
                         const directory = process.argv[3] || '/tmp/'
                         const filename = url.split("/files/")[1]
-                        const isFileValid = fs.existsSync(`${directory}/${filename}`)
-
-                        if (!isFileValid) {
-                            socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
-                        } else {
-                            const content = fs.readFileSync(`${directory}/${filename}`)
-                            const header = mountHeader(content)
-                            const response = `${responseOk}\r\n${header}\r\n${content.toString()}`
-
-                            socket.write(response)
-                        }
-                    }catch(e){
-                        socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+                        const filePath = `${directory}/${filename}`
+                        const req = data.toString().split("\r\n")
+                        const body = req[req.length - 1]
+                        fs.writeFileSync(filePath, body)
+                        socket.write('HTTP/1.1 201 Created\r\n\r\n')
                     }
-                } else {
-                    socket.write(`${responseNotFound}\r\n\r\n`)
-                }
             }
         }
     })
